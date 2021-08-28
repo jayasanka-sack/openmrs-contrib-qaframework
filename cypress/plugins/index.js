69fe76ad640a5d7af88f7c57c88b5f8752104264
jayasanka-sack <jayasanka.sack@gmail.com>
2021-08-28 17:07:58 +0530

    Save videos recordings of failing 3.x tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,13 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
+const del = require('del')
 
 module.exports = (on) => {
   on('file:preprocessor', cucumber());
+  on('after:spec', (spec, results) => {
+    if (results.stats.failures === 0 && results.video) {
+      // `del()` returns a promise, so it's important to return it to ensure
+      // deleting the video is finished before moving on
+      return del(results.video)
+    }
+  })
 }
